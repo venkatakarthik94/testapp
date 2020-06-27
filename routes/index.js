4345332c92aef085ad38ca5a00121044781ad007
karthik <38027604+venkatakarthik94@users.noreply.github.com>
2020-06-27 23:16:18 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Success  BOOM");
+  res.send("SuccessBOOM");
 });
 
 module.exports = router;
