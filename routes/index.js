1933f263e0bfa052baa5e0aea5be915efac63a0e
karthik <38027604+venkatakarthik94@users.noreply.github.com>
2020-06-27 22:45:43 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Success");
+  res.send("100% Success");
 });
 
 module.exports = router;
